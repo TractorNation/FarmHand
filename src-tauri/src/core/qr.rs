a458373d3b5c496dee61ab3b1addb7ae8a369fec
Henry Mullin <hdmullin1057@gmail.com>
2026-01-08 12:39:31 -0500

    Switched qr code generation crate

--- src-tauri/src/core/qr.rs
+++ src-tauri/src/core/qr.rs
@@ -1,11 +1,19 @@
+use qrcode::{render::svg, QrCode};
 use std::fs;
-
-use qrcode_generator::{QRCodeError, QrCodeEcc};
 use tauri::Error;
 
-pub fn encode_to_svg(data: &String) -> Result<std::string::String, QRCodeError> {
-    // Create SVG QR code
-    qrcode_generator::to_svg_to_string(data, QrCodeEcc::Low, 265, None::<&str>)
+pub fn encode_to_svg(data: &String) -> Result<String, String> {
+    // Generate QR code
+    let code =
+        QrCode::new(data.as_bytes()).map_err(|e| format!("Failed to generate QR code: {}", e))?;
+
+    let svg = code
+        .render::<svg::Color>()
+        .quiet_zone(true) // Disable automatic quiet zone
+        .min_dimensions(265, 265)
+        .build();
+
+    Ok(svg)
 }
 
 pub fn save_as_svg(svg: &String, file_path: &String) -> Result<(), Error> {
