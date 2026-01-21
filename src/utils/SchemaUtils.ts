424bd7c5abfbc47bc82d5f8f96ea177910396278
oGeyra <fob_mc@outlook.com>
2026-01-20 20:06:50 -0500

    Schema update v2

--- src/utils/SchemaUtils.ts
+++ src/utils/SchemaUtils.ts
@@ -9,25 +9,33 @@ import { createSchemaHash } from "./GeneralUtils";
 import { appLocalDataDir, resolve } from "@tauri-apps/api/path";
 import Reefscape from "../config/schema/2025Reefscape.json";
 import Decode from "../config/schema/2025Decode.json";
+import Pits from "../config/schema/2026PitScouting.json";
+import Rebuilt from "../config/schema/2026Rebuilt.json";
 import { invoke } from "@tauri-apps/api/core";
 
 export const defaultSchemas: SchemaMetaData[] = [
-  {
-    name: "2025 Decode",
-    path: "../config/schema/2025Decode.json",
-    schema: Decode as Schema,
-    type: "default",
-  },
-  {
-    name: "2025 Reefscape",
-    path: "../config/schema/2025Reefscape.json",
-    schema: Reefscape as Schema,
-    type: "default",
-  },
+  // {
+  //   name: "2025 Decode",
+  //   path: "../config/schema/2025Decode.json",
+  //   schema: Decode as Schema,
+  //   type: "default",
+  // },
+  // {
+  //   name: "2025 Reefscape",
+  //   path: "../config/schema/2025Reefscape.json",
+  //   schema: Reefscape as Schema,
+  //   type: "default",
+  // },
   {
     name: "2026 Pit Scouting",
-    path: "../config/schema/2025PitScouting.json",
-    schema: Reefscape as Schema,
+    path: "../config/schema/2026PitScouting.json",
+    schema: Pits as Schema,
+    type: "default",
+  },
+   {
+    name: "2026 Rebuilt",
+    path: "../config/schema/2026Rebuilt.json",
+    schema: Rebuilt as Schema,
     type: "default",
   }
 ];
